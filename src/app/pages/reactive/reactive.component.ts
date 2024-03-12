bed34573516c5467b8e6baf9bb9b2c0aa88a44b0
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-03-12 07:41:38 -0500

    Agrupación de los OBJETOS con formGroupName y validaciones para todos los campos del formulario con sus mensajes de error

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -26,11 +26,23 @@ export class ReactiveComponent {
     return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
   }
 
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito')?.invalid && this.forma.get('direccion.distrito')?.touched
+  }
+
+  get ciudadNoValido(){
+    return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
+  }
+
   crearFormulario(){
     this.forma = this.fb.group({
       nombre:['', [Validators.required, Validators.minLength(5)]],
       apellido:['', Validators.required],
-      correo:['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      correo:['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      direccion: this.fb.group({
+        distrito:['',Validators.required],
+        ciudad:['', Validators.required]
+      })
     })
   }
 
@@ -39,8 +51,13 @@ export class ReactiveComponent {
 
     if(this.forma.invalid){
       return Object.values(this.forma.controls).forEach(control => {
-        control.markAsTouched();
-      })
+
+        if(control instanceof FormGroup){
+          Object.values(control.controls).forEach(control => control.markAsTouched());
+         }else {
+           control.markAsTouched();
+        }
+      });
     }
   }
 
